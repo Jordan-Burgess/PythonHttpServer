f1acfce8625242ba08e0eb0ac7dadb45fe3571db
Jordan-Burgess <alfredjordan93@gmail.com>
2023-02-16 20:28:12 -0500

    Add main server file

---
+++ server.py
@@ -0,0 +1,31 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+
+    def do_POST(self):
+        self._set_headers()
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self.wfile.write(post_data)
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Server Started')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run() 
